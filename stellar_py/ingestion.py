350125373f26f5d1fd28c11b46e6f49147854e99
Kevin Jung <kevinjungdata61@github.com>
2018-02-14 16:44:35 +1100

    Updated names of classes and fields for consistency

--- stellar_py/ingestion.py
+++ stellar_py/ingestion.py
@@ -8,104 +8,104 @@ from stellar_py.payload import Payload
 from typing import Dict, Optional, List
 
 
-class VertexMapping:
+class NodeMapping:
     """Vertex mapping for data source
 
     Attributes:
-        vertex_class: class label
-        vertex_id: column name for unique ID
-        properties: dict of {key: column}
+        node_type: type
+        node_id: column name for unique ID
+        attributes: dict of {key: column}
     """
-    def __init__(self, vertex_class: str, vertex_id: str, properties: Dict[str, str]) -> None:
-        self.vertex_class = vertex_class
-        self.vertex_id = vertex_id
-        self.properties = properties or {}
+    def __init__(self, node_type: str, node_id: str, attributes: Dict[str, str]) -> None:
+        self.node_type = node_type
+        self.node_id = node_id
+        self.attributes = attributes or {}
 
 
 class EdgeMapping:
     """Edge mapping for data source
 
     Attributes:
-        edge_class: class label
-        src_class: source vertex class label
-        src: column name for source vertex
-        dst: column name for destination vertex
-        properties: dict of {key: column}
+        edge_type: type
+        src_type: source node type
+        src: column name for source node
+        dst: column name for destination node
+        attributes: dict of {key: column}
     """
-    def __init__(self, edge_class: str, src_class: str, src: str, dst: str, properties: Dict[str, str]) -> None:
-        self.edge_class = edge_class
-        self.src_class = src_class
+    def __init__(self, edge_type: str, src_type: str, src: str, dst: str, attributes: Dict[str, str]) -> None:
+        self.edge_type = edge_type
+        self.src_type = src_type
         self.src = src
         self.dst = dst
-        self.properties = properties or {}
+        self.attributes = attributes or {}
 
 
-class ElementClass:
+class ElementType:
     """Generic graph element class
 
     """
-    def __init__(self, name: str, properties: Dict[str, str]) -> None:
+    def __init__(self, name: str, attribute_types: Dict[str, str]) -> None:
         self.name = name
-        self.properties = properties or {}
+        self.attribute_types = attribute_types or {}
 
-    def validate_props(self, properties: Dict[str, str]):
-        """Require properties to be validated
+    def validate_attributes(self, attributes: Dict[str, str]):
+        """Require attributes to be validated
 
-        :param properties:  element properties
-        :return:            element properties
+        :param attributes:  element attributes
+        :return:            element attributes
         """
-        if not properties:
-            return properties
-        schema_props_keys = set(self.properties.keys())
-        mapping_props_keys = set(properties.keys())
+        if not attributes:
+            return attributes
+        schema_props_keys = set(self.attribute_types.keys())
+        mapping_props_keys = set(attributes.keys())
         if len(mapping_props_keys - schema_props_keys.intersection(mapping_props_keys)) > 0:
-            raise KeyError("invalid properties")
-        return properties
+            raise KeyError("invalid attributes")
+        return attributes
 
 
-class VertexClass(ElementClass):
+class NodeType(ElementType):
     """Vertex class for graph schema
 
     Attributes:
         name (str): Unique label name
-        properties (Dict[str, str]): dict of {key: type}
+        attribute_types (Dict[str, str]): dict of {key: type}
     """
-    def __init__(self, name: str, properties: Dict[str, str]) -> None:
-        ElementClass.__init__(self, name, properties)
+    def __init__(self, name: str, attribute_types: Dict[str, str]) -> None:
+        ElementType.__init__(self, name, attribute_types)
 
-    def create_mapping(self, vertex_id: str, properties: Optional[Dict[str, str]]=None) -> VertexMapping:
-        """Create vertex mapping
+    def create_mapping(self, node_id: str, attributes: Optional[Dict[str, str]]=None) -> NodeMapping:
+        """Create node mapping
 
-        :param vertex_id:   column name containing vertex ID
-        :param properties:  dict of property name to column name
-        :return:            vertex mapping
+        :param node_id:   column name containing node ID
+        :param attributes:  dict of property name to column name
+        :return:            node mapping
         """
-        return VertexMapping(self.name, vertex_id, self.validate_props(properties))
+        return NodeMapping(self.name, node_id, self.validate_attributes(attributes))
 
 
-class EdgeClass(ElementClass):
+class EdgeType(ElementType):
     """Edge class for graph schema
 
     Attributes:
         name (str): Unique label name
-        src_class (str): source vertex class label name
-        dst_class (str): destination vertex class label name
-        properties (Dict[str, str]): dict of {key: type}
+        src_type (str): source node type name
+        dst_type (str): destination node type name
+        attribute_types (Dict[str, str]): dict of {key: type}
     """
-    def __init__(self, name: str, src_class: str, dst_class: str, properties: Dict[str, str]) -> None:
-        ElementClass.__init__(self, name, properties)
-        self.src_class = src_class
-        self.dst_class = dst_class
+    def __init__(self, name: str, src_type: str, dst_type: str, attribute_types: Dict[str, str]) -> None:
+        ElementType.__init__(self, name, attribute_types)
+        self.src_type = src_type
+        self.dst_type = dst_type
 
-    def create_mapping(self, src: str, dst: str, properties: Optional[Dict[str, str]]=None) -> EdgeMapping:
+    def create_mapping(self, src: str, dst: str, attributes: Optional[Dict[str, str]]=None) -> EdgeMapping:
         """Create edge mapping
 
         :param src:         column name containing source ID
         :param dst:         column name containing destination ID
-        :param properties:  dict of property name to column name
+        :param attributes:  dict of property name to column name
         :return:            edge mapping
         """
-        return EdgeMapping(self.name, self.src_class, src, dst, self.validate_props(properties))
+        return EdgeMapping(self.name, self.src_type, src, dst, self.validate_attributes(attributes))
 
 
 class GraphSchema:
@@ -113,47 +113,47 @@ class GraphSchema:
 
     """
     def __init__(self):
-        self.vertex = {}
+        self.node = {}
         self.edge = {}
 
-    def add_vertex_class(self, name: str, properties: Optional[Dict[str, str]] = None) -> None:
-        """Add vertex class definition to schema
+    def add_node_type(self, name: str, attribute_types: Optional[Dict[str, str]] = None) -> None:
+        """Add node class definition to schema
 
-        :param name:        name of vertex class
-        :param properties:  dict of properties to their types
+        :param name:        name of node class
+        :param attribute_types:  dict of attributes to their types
         """
-        if name not in self.vertex.keys():
-            self.vertex[name] = VertexClass(name, properties)
+        if name not in self.node.keys():
+            self.node[name] = NodeType(name, attribute_types)
 
-    def add_edge_class(self, name: str, src_class: str, dst_class: str,
-                       properties: Optional[Dict[str, str]] = None) -> None:
+    def add_edge_type(self, name: str, src_type: str, dst_type: str,
+                      attribute_types: Optional[Dict[str, str]] = None) -> None:
         """Add edge class definition to schema
 
         :param name:        name of edge class
-        :param src_class:   name of source vertex class
-        :param dst_class:   name of destination vertex class
-        :param properties:  dict of properties to their types
+        :param src_type:   name of source node class
+        :param dst_type:   name of destination node class
+        :param attribute_types:  dict of attributes to their types
         """
         if name not in self.edge.keys():
-            self.edge[name] = EdgeClass(name, src_class, dst_class, properties)
+            self.edge[name] = EdgeType(name, src_type, dst_type, attribute_types)
 
 
 class DataSource:
-    """Used to define a data source and its column mappings as graph elements and properties
+    """Used to define a data source and its column mappings as graph elements and attributes
 
     """
-    def __init__(self, path: str, vertex_mappings: Optional[List[VertexMapping]] = None,
+    def __init__(self, path: str, node_mappings: Optional[List[NodeMapping]] = None,
                  edge_mappings: Optional[List[EdgeMapping]] = None) -> None:
         self.path = path
-        self.vertex_mappings = vertex_mappings or []
+        self.node_mappings = node_mappings or []
         self.edge_mappings = edge_mappings or []
 
-    def add_vertex_mapping(self, vertex_mapping: VertexMapping) -> None:
-        """Add a vertex mapping
+    def add_node_mapping(self, node_mapping: NodeMapping) -> None:
+        """Add a node mapping
 
-        :param vertex_mapping: vertex mapping
+        :param node_mapping: node mapping
         """
-        self.vertex_mappings.append(vertex_mapping)
+        self.node_mappings.append(node_mapping)
 
     def add_edge_mapping(self, edge_mapping: EdgeMapping) -> None:
         """Add an edge mapping
@@ -175,64 +175,64 @@ class StellarIngestPayload(Payload):
         Payload.__init__(self, session_id, label)
         self.sources = [s.path for s in sources]
         self.graphSchema = {
-            "classes": [self.vc2c(vc) for vc in schema.vertex.values()],
-            "classLinks": [self.ec2cl(ec) for ec in schema.edge.values()]
+            "classes": [self.nt2c(vc) for vc in schema.node.values()],
+            "classLinks": [self.et2cl(ec) for ec in schema.edge.values()]
         }
         self.mapping = {
             "nodes": [node for s in sources
                       for node in
-                      self.vms2nodes(s.path, s.vertex_mappings)],
+                      self.nms2nodes(s.path, s.node_mappings)],
             "links": [link for s in sources
                       for link in
                       self.ems2links(s.path, s.edge_mappings)]
         }
 
     @staticmethod
-    def vc2c(vertex_class: VertexClass) -> Dict[str, str]:
+    def nt2c(node_type: NodeType) -> Dict[str, str]:
         """Vertex class to payload "class" element
 
-        :param vertex_class:    vertex class
+        :param node_type:    node class
         :return:                "class" element in payload
         """
         return {
-            "name": vertex_class.name,
-            "props": vertex_class.properties
+            "name": node_type.name,
+            "props": node_type.attribute_types
         }
 
     @staticmethod
-    def ec2cl(edge_class: EdgeClass) -> Dict[str, str]:
+    def et2cl(edge_type: EdgeType) -> Dict[str, str]:
         """Edge class to payload "class link" element
 
-        :param edge_class:  edge class
+        :param edge_type:  edge class
         :return:            "class link" element in payload
         """
         return {
-            "name": edge_class.name,
-            "source": edge_class.src_class,
-            "target": edge_class.dst_class,
-            "props": edge_class.properties
+            "name": edge_type.name,
+            "source": edge_type.src_type,
+            "target": edge_type.dst_type,
+            "props": edge_type.attribute_types
         }
 
     @staticmethod
-    def vms2nodes(source: str, vms: List[VertexMapping]) -> List[Dict]:
+    def nms2nodes(source: str, vms: List[NodeMapping]) -> List[Dict]:
         """Vertex mappings to "nodes" element in payload
 
         :param source:  data source path
-        :param vms:     vertex mappings
+        :param vms:     node mappings
         :return:        "nodes" element in payload
         """
-        def vm2node(vm: VertexMapping) -> Dict:
+        def nm2node(vm: NodeMapping) -> Dict:
             """Vertex mapping to "node"
 
-            :param vm:  vertex mapping
+            :param vm:  node mapping
             :return:    "node" element in payload
             """
             node = {k: {"column": v, "source": source}
-                    for k, v in vm.properties.items()}
-            node["@id"] = {"column": vm.vertex_id, "source": source}
-            node["@type"] = vm.vertex_class
+                    for k, v in vm.attributes.items()}
+            node["@id"] = {"column": vm.node_id, "source": source}
+            node["@type"] = vm.node_type
             return node
-        return [vm2node(vm) for vm in vms]
+        return [nm2node(vm) for vm in vms]
 
     @staticmethod
     def ems2links(source: str, ems: List[EdgeMapping]) -> List[Dict]:
@@ -249,10 +249,10 @@ class StellarIngestPayload(Payload):
             :return:    "link" element in payload
             """
             link = {k: {"column": v, "source": source}
-                    for k, v in em.properties.items()}
+                    for k, v in em.attributes.items()}
             link["@src"] = {"column": em.src, "source": source}
             link["@dest"] = {"column": em.dst, "source": source}
-            link["@type"] = {"name": em.edge_class, "source": em.src_class}
+            link["@type"] = {"name": em.edge_type, "source": em.src_type}
             return link
         return [em2link(em) for em in ems]
 
--- tests/test_ingestion.py
+++ tests/test_ingestion.py
@@ -12,12 +12,12 @@ stellar_addr_session = stellar_addr + "/session/create"
 @pytest.fixture(scope="module")
 def graph_schema():
     schema = create_graph_schema()
-    schema.add_vertex_class(name='src vertex')
-    schema.add_vertex_class(name='dst vertex', properties={})
-    schema.add_vertex_class(name='dst vertex 2', properties={'number': 'integer'})
-    schema.add_edge_class(name='edge', src_class='src vertex', dst_class='dst vertex')
-    schema.add_edge_class(name='edge 2', src_class='src vertex', dst_class='dst vertex 2',
-                          properties={'str': 'string'})
+    schema.add_node_type(name='src node')
+    schema.add_node_type(name='dst node', attribute_types={})
+    schema.add_node_type(name='dst node 2', attribute_types={'number': 'integer'})
+    schema.add_edge_type(name='edge', src_type='src node', dst_type='dst node')
+    schema.add_edge_type(name='edge 2', src_type='src node', dst_type='dst node 2',
+                         attribute_types={'str': 'string'})
     return schema
 
 
@@ -25,39 +25,39 @@ def graph_schema():
 def data_source():
     schema = graph_schema()
     source = new_data_source("path")
-    source.add_vertex_mapping(schema.vertex['src vertex'].create_mapping(vertex_id="SrcId"))
-    source.add_vertex_mapping(schema.vertex['dst vertex'].create_mapping(vertex_id="DstId",
-                                                                         properties={}))
-    source.add_vertex_mapping(schema.vertex['dst vertex 2'].create_mapping(
-        vertex_id="DstId2", properties={'number': 'Number'}))
+    source.add_node_mapping(schema.node['src node'].create_mapping(node_id="SrcId"))
+    source.add_node_mapping(schema.node['dst node'].create_mapping(node_id="DstId",
+                                                                     attributes={}))
+    source.add_node_mapping(schema.node['dst node 2'].create_mapping(
+        node_id="DstId2", attributes={'number': 'Number'}))
     source.add_edge_mapping(schema.edge['edge'].create_mapping(src="SrcId", dst="DstId"))
     source.add_edge_mapping(schema.edge['edge 2'].create_mapping(src="SrcId", dst="DstId2",
-                                                                 properties={'str': 'Address'}))
+                                                                 attributes={'str': 'Address'}))
     return source
 
 
 def test_graph_schema():
     schema = graph_schema()
-    assert schema.vertex['src vertex'].name == 'src vertex'
-    assert schema.vertex['src vertex'].properties == {}
-    assert schema.vertex['dst vertex'].name == 'dst vertex'
-    assert schema.vertex['dst vertex'].properties == {}
-    assert schema.vertex['dst vertex 2'].name == 'dst vertex 2'
-    assert schema.vertex['dst vertex 2'].properties == {'number': 'integer'}
+    assert schema.node['src node'].name == 'src node'
+    assert schema.node['src node'].attribute_types == {}
+    assert schema.node['dst node'].name == 'dst node'
+    assert schema.node['dst node'].attribute_types == {}
+    assert schema.node['dst node 2'].name == 'dst node 2'
+    assert schema.node['dst node 2'].attribute_types == {'number': 'integer'}
     assert schema.edge['edge'].name == 'edge'
-    assert schema.edge['edge'].src_class == 'src vertex'
-    assert schema.edge['edge'].dst_class == 'dst vertex'
-    assert schema.edge['edge'].properties == {}
+    assert schema.edge['edge'].src_type == 'src node'
+    assert schema.edge['edge'].dst_type == 'dst node'
+    assert schema.edge['edge'].attribute_types == {}
     assert schema.edge['edge 2'].name == 'edge 2'
-    assert schema.edge['edge 2'].src_class == 'src vertex'
-    assert schema.edge['edge 2'].dst_class == 'dst vertex 2'
-    assert schema.edge['edge 2'].properties == {'str': 'string'}
+    assert schema.edge['edge 2'].src_type == 'src node'
+    assert schema.edge['edge 2'].dst_type == 'dst node 2'
+    assert schema.edge['edge 2'].attribute_types == {'str': 'string'}
 
 
 def test_data_source():
     source = data_source()
     assert source.path == "path"
-    assert len(source.vertex_mappings) == 3
+    assert len(source.node_mappings) == 3
     assert len(source.edge_mappings) == 2
 
 
@@ -65,11 +65,11 @@ def test_data_source_invalid_properties():
     schema = graph_schema()
     source = new_data_source("path")
     with pytest.raises(KeyError):
-        source.add_vertex_mapping(
-            schema.vertex['src vertex'].create_mapping(vertex_id="SrcId", properties={'number': 'Number'}))
+        source.add_node_mapping(
+            schema.node['src node'].create_mapping(node_id="SrcId", attributes={'number': 'Number'}))
     with pytest.raises(KeyError):
         source.add_edge_mapping(schema.edge['edge 2'].create_mapping(src='SrcId', dst='DstId2',
-                                                                     properties={'str_bad': 'Address'}))
+                                                                     attributes={'str_bad': 'Address'}))
 
 
 def test_ingest_payload():
@@ -79,41 +79,41 @@ def test_ingest_payload():
     assert payload.sessionId == "id"
     assert payload.sources == ['path']
     assert payload.label == 'test_ingest_payload'
-    assert len(payload.graphSchema['classes']) == len(schema.vertex)
+    assert len(payload.graphSchema['classes']) == len(schema.node)
     assert len(payload.graphSchema['classLinks']) == len(schema.edge)
-    assert len(payload.mapping['nodes']) == len(source.vertex_mappings)
+    assert len(payload.mapping['nodes']) == len(source.node_mappings)
     assert len(payload.mapping['links']) == len(source.edge_mappings)
 
 
 def test_ingest_payload_vc2c():
-    vc = graph_schema().vertex['dst vertex 2']
-    c = StellarIngestPayload.vc2c(vc)
+    vc = graph_schema().node['dst node 2']
+    c = StellarIngestPayload.nt2c(vc)
     assert vc.name == c['name']
-    assert vc.properties == c['props']
-    vc_noprops = graph_schema().vertex['src vertex']
-    c_noprops = StellarIngestPayload.vc2c(vc_noprops)
+    assert vc.attribute_types == c['props']
+    vc_noprops = graph_schema().node['src node']
+    c_noprops = StellarIngestPayload.nt2c(vc_noprops)
     assert vc_noprops.name == c_noprops['name']
     assert c_noprops['props'] == {}
 
 
 def test_ingest_payload_ec2cl():
     ec = graph_schema().edge['edge 2']
-    cl = StellarIngestPayload.ec2cl(ec)
+    cl = StellarIngestPayload.et2cl(ec)
     assert ec.name == cl['name']
-    assert ec.properties == cl['props']
+    assert ec.attribute_types == cl['props']
     ec_noprops = graph_schema().edge['edge']
-    cl_noprops = StellarIngestPayload.ec2cl(ec_noprops)
+    cl_noprops = StellarIngestPayload.et2cl(ec_noprops)
     assert ec_noprops.name == cl_noprops['name']
     assert cl_noprops['props'] == {}
 
 
 def test_ingest_payload_vms2nodes():
     src = new_data_source('path')
-    src.add_vertex_mapping(graph_schema().vertex['dst vertex 2'].create_mapping(vertex_id='Id',
-                                                                                properties={'number': 'Number'}))
-    nodes = StellarIngestPayload.vms2nodes('path', src.vertex_mappings)
+    src.add_node_mapping(graph_schema().node['dst node 2'].create_mapping(node_id='Id',
+                                                                            attributes={'number': 'Number'}))
+    nodes = StellarIngestPayload.nms2nodes('path', src.node_mappings)
     assert nodes[0]['@id'] == {'column': 'Id', 'source': 'path'}
-    assert nodes[0]['@type'] == 'dst vertex 2'
+    assert nodes[0]['@type'] == 'dst node 2'
     assert nodes[0]['number'] == {'column': 'Number', 'source': 'path'}
 
 
@@ -121,11 +121,11 @@ def test_ingest_payload_ems2links():
     src = new_data_source('path')
     src.add_edge_mapping(graph_schema().edge['edge 2'].create_mapping(src='SrcId',
                                                                       dst='DstId',
-                                                                      properties={'str': 'Address'}))
+                                                                      attributes={'str': 'Address'}))
     links = StellarIngestPayload.ems2links('path', src.edge_mappings)
     assert links[0]['@src'] == {'column': 'SrcId', 'source': 'path'}
     assert links[0]['@dest'] == {'column': 'DstId', 'source': 'path'}
-    assert links[0]['@type'] == {'name': 'edge 2', 'source': 'src vertex'}
+    assert links[0]['@type'] == {'name': 'edge 2', 'source': 'src node'}
     assert links[0]['str'] == {'column': 'Address', 'source': 'path'}
 
 
