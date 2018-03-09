7cef164b44e47d732074448a56321a59f66077b5
kevin <33508488+KevinJungData61@users.noreply.github.com>
2018-03-09 17:21:34 +1100

    Added copyright notice to all source files

--- setup.py
+++ setup.py
@@ -1,3 +1,24 @@
+__copyright__ = """
+
+    This file is part of stellar-py, Stellar Python Client.
+
+    Copyright 2018 CSIRO Data61
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+    Unless required by applicable law or agreed to in writing, software
+    distributed under the License is distributed on an "AS IS" BASIS,
+    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+    See the License for the specific language governing permissions and
+    limitations under the License.
+
+"""
+__license__ = "Apache 2.0"
+
 from setuptools import setup
 from setuptools import find_packages
 
