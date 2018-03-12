5508b86364e72a105aa928fb33bc86df14c2e7bf
kevin <33508488+KevinJungData61@users.noreply.github.com>
2018-03-12 14:21:36 +1100

    Updated repository URL

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ from setuptools import find_packages
 setup(name='stellar-py',
       version='0.2.1',
       description='Python client for the Stellar project',
-      url='serene.sh',
+      url='https://github.com/data61/stellar-py',
       author='Kevin Jung',
       author_email='kevin.jung@data61.csiro.au',
       license='Apache 2.0',
